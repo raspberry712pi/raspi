f75db8a6d15a55dd7c012821d0f448983cfc5a2f
raspberry712pi <guptaayush08335@gmail.com>
2024-10-22 08:59:34 +0530

    Update picam.py

--- picam.py
+++ picam.py
@@ -2,8 +2,17 @@ from picamera import PiCamera
 from time import sleep
 
 camera = PiCamera()
-camera.start_preview()	#start camera
-sleep(5)
-camera.capture('/home/pi/Desktop/image.jpg')	# take a picture
-camera.stop_preview()	# stop camera
 
+camera.start_preview()  
+
+
+sleep(2)  
+camera.capture('/home/pi/Desktop/image.jpg')  
+print("Photo captured!")
+
+camera.start_recording('/home/pi/Desktop/video.h264')  
+sleep(10)  
+camera.stop_recording()  
+print("Video recorded!")
+
+camera.stop_preview()  
